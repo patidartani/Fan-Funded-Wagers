5c98f5df358b746440bb1ef32bc454d113c96051
Tanisha Patidar <tanishapatidar905@gmail.com>
2025-06-02 15:33:15 +0530

    last commit

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ import { store, persistor } from "./redux-toolkit/store.js";
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/migrate">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </PersistGate>
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/migrate",  // 👈 Yeh ensure karega ki saare assets `/migrate/` ke andar serve ho
 });
